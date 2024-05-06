e3e793ddcf0de2a1548f9159d8b208926cb26b6a
Tatiana Titova <ritasom666@gmail.com>
2024-05-06 20:22:11 +0400

    chenge ex.2

--- src/test/java/ru/netology/test/TestMode.java
+++ src/test/java/ru/netology/test/TestMode.java
@@ -4,25 +4,61 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
+import static ru.netology.test.UserGenerator.getUser;
 import static ru.netology.test.UserGenerator.registerUser;
 
 public class TestMode {
-@BeforeEach
-void setUp(){
-    open("http://localhost:9999/");
-}
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
+
     @Test
-void successAuthorization(){
+    void successAuthorization() {
         var registerUs = registerUser("active");
         $("[data-test-id='login'] .input__control").sendKeys(registerUs.getLogin());
         $("[data-test-id='password'] .input__control").sendKeys(registerUs.getPassword());
-        $("[data-test-id='action-login'] .button").click();
-        $(withText("  Личный кабинет")).shouldBe(Condition.visible);
+        $("[data-test-id='action-login']").click();
+        $(withText("Личный кабинет")).shouldBe(Condition.visible);
     }
+
+    @Test
+    void notSuccessAuthorization() {
+        var user = getUser("active");
+        $("[data-test-id='login'] .input__control").sendKeys(user.getLogin());
+        $("[data-test-id='password'] .input__control").sendKeys(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void blockedUser() {
+        var registerUs = registerUser("blocked");
+        $("[data-test-id='login'] .input__control").sendKeys(registerUs.getLogin());
+        $("[data-test-id='password'] .input__control").sendKeys(registerUs.getPassword());
+        $("[data-test-id='action-login']").click();
+        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible);
+    }
+    @Test
+    void wrongLogin(){
+        var user = registerUser("active");
+        $("[data-test-id='login'] .input__control").sendKeys("Kolya");
+        $("[data-test-id='password'] .input__control").sendKeys(user.getPassword());
+        $("[data-test-id='action-login']").click();
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void wrongPassword(){
+        var user = registerUser("active");
+        $("[data-test-id='login'] .input__control").sendKeys(user.getLogin());
+        $("[data-test-id='password'] .input__control").sendKeys("password");
+        $("[data-test-id='action-login']").click();
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+
 }
+
--- src/test/java/ru/netology/test/UserGenerator.java
+++ src/test/java/ru/netology/test/UserGenerator.java
@@ -5,20 +5,17 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.openqa.selenium.devtools.v85.serviceworker.model.RegistrationID;
+
+import lombok.Value;
+
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 
-@Data
 
 public class UserGenerator {
-private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
             .setAccept(ContentType.JSON)
@@ -26,42 +23,44 @@ private static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .log(LogDetail.ALL)
             .build();
     private static final Faker faker = new Faker(new Locale("en"));
-    private UserGenerator(){}
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
+    private UserGenerator() {
+    }
+
+    @Value
     public static class UserDto {
-        private  String login;
-        private  String password;
-        private  String status;
+        String login;
+        String password;
+        String status;
 
 
     }
 
-private static UserDto sendReq(UserDto user){
-    given() // "дано"
-            .spec(requestSpec) // указываем, какую спецификацию используем
-            .body(user) // передаём в теле объект, который будет преобразован в JSON
-            .when() // "когда"
-            .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
-            .then() // "тогда ожидаем"
-            .statusCode(200); // код 200 OK
-    return user;
-}
-public static String RandomLogin(){
+    private static UserDto sendReq(UserDto user) {
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+        return user;
+    }
+
+    public static String randomLogin() {
         return faker.name().firstName();
-}
+    }
 
-public static String RandomPassword(){
+    public static String randomPassword() {
         return faker.lorem().characters(8);
-}
+    }
 
-public static UserDto getUser(String status){
-        return new UserDto(RandomLogin(), RandomPassword(), status);
-}
+    public static UserDto getUser(String status) {
+        return new UserDto(randomLogin(), randomPassword(), status);
+    }
 
-public static UserDto registerUser(String status){
+    public static UserDto registerUser(String status) {
         return sendReq(getUser(status));
-}
+    }
 
 }
